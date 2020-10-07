68993865bcd96a5daea4c5e6334050d9c2b1f8db
Matthew Jeorrett <Matthew@wallet.services>
2020-10-07 22:31:28 +0100

    simplify return from race stages controller

--- StageRaceFantasy/Shared/Models/RaceStages/GetRaceStageDto.cs
+++ StageRaceFantasy/Shared/Models/RaceStages/GetRaceStageDto.cs
@@ -3,8 +3,7 @@
     public class GetRaceStageDto
     {
         public int Id { get; set; }
-        public string RaceName { get; set; }
-        public string StageStartLocation { get; set; }
-        public string StageFinishLocation { get; set; }
+        public string StartLocation { get; set; }
+        public string FinishLocation { get; set; }
     }
 }
