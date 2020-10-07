f8d77fe240f0bd0efc5ab95b8d5eec07ad397e4a
Matthew Jeorrett <Matthew@wallet.services>
2020-10-07 22:20:56 +0100

    add race stages controller get, get all and create

---
+++ StageRaceFantasy/Server/Commands/RaceStages/CreateRaceStageCommand.cs
@@ -0,0 +1,9 @@
+﻿using StageRaceFantasy.Shared.Models;
+
+namespace StageRaceFantasy.Server.Commands.RaceStages
+{
+    public record CreateRaceStageCommand(int RaceId, string StartLocation, string FinishLocation)
+        : IApplicationCommand<GetRaceStageDto>
+    {
+    }
+}
---
+++ StageRaceFantasy/Server/Queries/RaceStages/GetRaceStagesHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using StageRaceFantasy.Server.Db;
+using StageRaceFantasy.Shared.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StageRaceFantasy.Server.Queries.GetRaceStages
+{
+    public class GetRaceStagesHandler : IApplicationQueryHandler<GetRaceStagesQuery, GetRaceStagesDto>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetRaceStagesHandler(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<QueryResult<GetRaceStagesDto>> Handle(GetRaceStagesQuery request, CancellationToken cancellationToken)
+        {
+            var raceId = request.RaceId;
+
+            var race = await _dbContext.Races
+                .Include(x => x.Stages)
+                .FirstOrDefaultAsync(x => x.Id == raceId);
+
+            if (race == null)
+            {
+                return new()
+                {
+                    IsNotFound = true,
+                };
+            }
+
+            return new(_mapper.Map<GetRaceStagesDto>(race));
+        }
+    }
+}
