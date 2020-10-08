dc78ba3a8c62cde31b91fe23ea8c59b1a834178d
Matthew Jeorrett <Matthew@wallet.services>
2020-10-08 23:22:31 +0100

    refactor create fantasy team command

--- StageRaceFantasy.Application/Commands/FantasyTeams/CreateFantasyTeamCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using StageRaceFantasy.Application.Common.Mediatr;
-using StageRaceFantasy.Domain.Entities;
-
-namespace StageRaceFantasy.Application.Commands
-{
-    public record CreateFantasyTeamCommand(string Name) : IApplicationCommand<FantasyTeam>
-    {
-    }
-}
