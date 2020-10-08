5f15db9bdaf59020f2e85cc11c9a4f16e8e872f8
Matthew Jeorrett <Matthew@wallet.services>
2020-10-09 00:10:24 +0100

    refactor get fantasy team query

---
+++ StageRaceFantasy.Application/FantasyTeams/Queries/GetById/GetFantasyTeamVm.cs
@@ -0,0 +1,19 @@
+﻿using StageRaceFantasy.Application.Common.AutoMapper;
+using StageRaceFantasy.Domain.Entities;
+using System.Collections.Generic;
+
+namespace StageRaceFantasy.Application.FantasyTeams.Queries.GetById
+{
+    public record GetFantasyTeamVm : IMapFrom<FantasyTeam>
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public List<FantasyTeamRaceEntryDto> RaceEntries { get; init; }
+    }
+
+    public record FantasyTeamRaceEntryDto : IMapFrom<FantasyTeamRaceEntry>
+    {
+        public int RaceId { get; init; }
+        public string RaceName { get; init; }
+    }
+}
--- StageRaceFantasy.Application/Queries/FantasyTeams/GetFantasyTeamQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using StageRaceFantasy.Application.Common.Mediatr;
-using StageRaceFantasy.Domain.Entities;
-
-namespace StageRaceFantasy.Application.Queries
-{
-    public record GetFantasyTeamQuery(int TeamId) : IApplicationQuery<GetFantasyTeamDto>
-    {
-    }
-}
