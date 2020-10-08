addbef18e7501fa15c6699f7a9f2d3e518d79648
Matthew Jeorrett <Matthew@wallet.services>
2020-10-08 18:38:51 +0100

    edit race stage

---
+++ StageRaceFantasy/Server/Commands/RaceStages/UpdateRaceStageCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace StageRaceFantasy.Server.Commands.RaceStages
+{
+    public record UpdateRaceStageCommand : IApplicationCommand
+    {
+        public int RaceId { get; init; }
+        public int StageId { get; init; }
+        public string StartLocation { get; init; }
+        public string FinishLocation { get; init; }
+    }
+}
