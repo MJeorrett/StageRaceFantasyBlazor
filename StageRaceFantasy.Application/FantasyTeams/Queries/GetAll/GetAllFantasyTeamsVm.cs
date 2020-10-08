a0f08a40e21815d9a8ba48765e1e6f0226f04480
Matthew Jeorrett <Matthew@wallet.services>
2020-10-08 23:18:46 +0100

    refactor

--- StageRaceFantasy.Application/FantasyTeams/Queries/GetAll/GetAllFantasyTeamsVm.cs
+++ StageRaceFantasy.Application/FantasyTeams/Queries/GetAll/GetAllFantasyTeamsVm.cs
@@ -1,12 +1,19 @@
-﻿using StageRaceFantasy.Application.Common.AutoMapper;
+﻿using AutoMapper;
+using StageRaceFantasy.Application.Common.AutoMapper;
 using StageRaceFantasy.Domain.Entities;
 using System.Collections.Generic;
 
 namespace StageRaceFantasy.Application.FantasyTeams.Queries.GetAll
 {
-    public class GetAllFantasyTeamsVm
+    public class GetAllFantasyTeamsVm : IMapFrom<List<FantasyTeam>>
     {
         public List<FantasyTeamDto> FantasyTeams { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<List<FantasyTeam>, GetAllFantasyTeamsVm>()
+                .ForMember(dest => dest.FantasyTeams, opts => opts.MapFrom(source => source));
+        }
     }
 
     public class FantasyTeamDto : IMapFrom<FantasyTeam>
