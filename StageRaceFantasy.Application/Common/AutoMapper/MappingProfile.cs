5ce25d442a220f151b20c1b4aebd64daa2cda605
Matthew Jeorrett <Matthew@wallet.services>
2020-10-09 23:15:33 +0100

    Add non-generic IMapFrom Use to make it cleaner when you are implementing Mapping from multiple types

--- StageRaceFantasy.Application/Common/AutoMapper/MappingProfile.cs
+++ StageRaceFantasy.Application/Common/AutoMapper/MappingProfile.cs
@@ -15,20 +15,36 @@ namespace StageRaceFantasy.Application.Common.AutoMapper
         private void ApplyMappingsFromAssembly(Assembly assembly)
         {
             var types = assembly.GetExportedTypes()
-                .Where(t => t.GetInterfaces().Any(i =>
-                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .Where(t => t.GetInterfaces().Any(IsIMapperType))
                 .ToList();
 
             foreach (var type in types)
             {
                 var instance = Activator.CreateInstance(type);
 
-                var methodInfo = type.GetMethod("Mapping")
-                    ?? type.GetInterface("IMapFrom`1").GetMethod("Mapping");
+                var methodInfo = GetMappingMethodInfo(type);
 
                 methodInfo?.Invoke(instance, new object[] { this });
-
             }
+        }
+
+        private static MethodInfo GetMappingMethodInfo(Type type)
+        {
+            return type.GetMethod("Mapping")
+                ?? type.GetInterface("IMapFrom")?.GetMethod("Mapping")
+                ?? type.GetInterface("IMapFrom`1").GetMethod("Mapping");
+        }
+
+        private static bool IsIMapperType(Type i)
+        {
+            if (i == typeof(IMapFrom)) return true;
+
+            if (i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
