c4b91c46715146c20a2867e6d1bbb2084d7e1674
Matthew Jeorrett <Matthew@wallet.services>
2020-10-10 21:36:54 +0100

    add test

--- StageRaceFantasy.Application.Test/RaceStages/Commands/CreateRaceStageCommandTests.cs
+++ StageRaceFantasy.Application.Test/RaceStages/Commands/CreateRaceStageCommandTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using StageRaceFantasy.Application.RaceStages.Commands.Create;
+using StageRaceFantasy.Domain.Entities;
 using System.Threading.Tasks;
 
 namespace StageRaceFantasy.Application.IntegrationTests.RaceStages.Commands
@@ -20,6 +21,21 @@ namespace StageRaceFantasy.Application.IntegrationTests.RaceStages.Commands
             result.ValidationFailures[nameof(CreateRaceStageCommand.RaceId)].Should().Contain(x => x.Contains("must not be empty"));
             result.ValidationFailures[nameof(CreateRaceStageCommand.StartLocation)].Should().Contain(x => x.Contains("must not be empty"));
             result.ValidationFailures[nameof(CreateRaceStageCommand.FinishLocation)].Should().Contain(x => x.Contains("must not be empty"));
+        }
+        
+        [Test]
+        public async Task ShouldReturnBadRequestWhenRaceDoesNotExist()
+        {
+            var command = new CreateRaceStageCommand()
+            {
+                RaceId = 123,
+                StartLocation = "Paris",
+                FinishLocation = "Edinburgh",
+            };
+
+            var result = await SendAsync(command);
+
+            result.IsNotFound.Should().BeTrue();
         }
     }
 }
