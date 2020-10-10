a2490ab2a8dc03ea6462a262fe5e86f32a6628ec
Matthew Jeorrett <Matthew@wallet.services>
2020-10-10 22:11:13 +0100

    add create race command

--- StageRaceFantasy.Application.Test/RaceStages/Commands/CreateRaceStageCommandTests.cs
+++ StageRaceFantasy.Application.Test/RaceStages/Commands/CreateRaceStageCommandTests.cs
@@ -1,7 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using StageRaceFantasy.Application.RaceStages.Commands.Create;
-using StageRaceFantasy.Domain.Entities;
 using System.Threading.Tasks;
 
 namespace StageRaceFantasy.Application.IntegrationTests.RaceStages.Commands
@@ -11,16 +10,16 @@ namespace StageRaceFantasy.Application.IntegrationTests.RaceStages.Commands
     public class CreateRaceStageCommandTests : TestBase
     {
         [Test]
-        public async Task ShouldRequireFields()
+        public async Task ShouldRequireMinimumFields()
         {
             var command = new CreateRaceStageCommand();
 
             var result = await SendAsync(command);
 
             result.IsBadRequest.Should().BeTrue();
-            result.ValidationFailures[nameof(CreateRaceStageCommand.RaceId)].Should().Contain(x => x.Contains("must not be empty"));
-            result.ValidationFailures[nameof(CreateRaceStageCommand.StartLocation)].Should().Contain(x => x.Contains("must not be empty"));
-            result.ValidationFailures[nameof(CreateRaceStageCommand.FinishLocation)].Should().Contain(x => x.Contains("must not be empty"));
+            result.ValidationFailures[nameof(CreateRaceStageCommand.RaceId)].Should().Contain(x => x.Contains(ValidationMessageFragments.NotEmpty));
+            result.ValidationFailures[nameof(CreateRaceStageCommand.StartLocation)].Should().Contain(x => x.Contains(ValidationMessageFragments.NotEmpty));
+            result.ValidationFailures[nameof(CreateRaceStageCommand.FinishLocation)].Should().Contain(x => x.Contains(ValidationMessageFragments.NotEmpty));
         }
         
         [Test]
