0040020b66c8a1f938aa580bad7f721fa5b3fe75
Matthew Jeorrett <Matthew@wallet.services>
2020-10-10 21:13:23 +0100

    implement clean testing set up

---
+++ StageRaceFantasy.Application.Test/RaceStages/Commands/CreateRaceStageCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StageRaceFantasy.Application.RaceStages.Commands.Create;
+using System.Threading.Tasks;
+
+namespace StageRaceFantasy.Application.IntegrationTests.RaceStages.Commands
+{
+    using static Testing;
+
+    public class CreateRaceStageCommandTests : TestBase
+    {
+        [Test]
+        public async Task ShouldRequireTitle()
+        {
+            var command = new CreateRaceStageCommand();
+
+            var result = await SendAsync(command);
+
+            result.IsBadRequest.Should().BeTrue();
+        }
+    }
+}
